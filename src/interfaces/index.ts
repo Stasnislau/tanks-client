46712bd58a51e50085b06ffd2139a17b827050a3
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-04-23 21:43:56 +0200

    updated interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -13,5 +13,9 @@ export interface mapInterface {
 export interface tileInterface {
   x: number;
   y: number;
-  occupation: number; // 0 - empty, 1 - player, 2 - ai, 3 - bullet (player), 4 - bullet (ai), 5 - wall
+  occupation: string; // empty, player, ai, player-bullet, ai-bullet, wall
+}
+
+export interface MapComponentProps {
+  map: mapInterface;
 }
