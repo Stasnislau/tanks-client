7c402b5d7d8f41b0347003dc9d3caecc1e790c99
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-05-21 17:39:07 +0200

    implemented method for moving bullets

--- src/pages/api/logic/bullet/index.ts
+++ src/pages/api/logic/bullet/index.ts
@@ -22,13 +22,36 @@ class Bullet {
     this.y = y;
     this.direction = direction;
   }
-
+  //// move 3 tiles in the direction. Check all the tiles on the way if they are empty or not. If not, return the coordinates of the tile that is not empty and id of the bullet. If all tiles are empty, return null.
   moveBullet = (map: mapInterface, direction: string) => {
-    const xAfter =
-      this.x + (direction === "right" ? 3 : direction === "left" ? -3 : 0);
-    const yAfter =
-      this.y + (direction === "down" ? 3 : direction === "up" ? -3 : 0);
+    let x = this.x;
+    let y = this.y;
+    let i = 0;
+    while (i < 3) {
+      switch (direction) {
+        case "up":
+          y--;
+          break;
+        case "down":
+          y++;
+          break;
+        case "left":
+          x--;
+          break;
+        case "right":
+          x++;
+          break;
+      }
+      if (
+        map.tiles[y][x].occupation !== "empty" &&
+        map.tiles[y][x].occupation !== "bullet"
+      ) {
+        return { x, y, id: this.id };
+      }
+      i++;
+    }
+    return null;
   };
-} 
+}
 
 export default Bullet;
