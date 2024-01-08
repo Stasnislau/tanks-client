9a86cc5ba2d201b982f11c3e39bab5f5c59e6d1b
Stanislau Ryzhkou <stanislaurv@gmail.com>
2024-01-08 22:25:55 +0100

    Refactor game over and greetings modals

--- src/components/gameOverModal.tsx
+++ src/components/gameOverModal.tsx
@@ -6,33 +6,37 @@ interface GameOverModalProps {
 
 const GameOverModal = ({ isOpen, onRestart, outcome }: GameOverModalProps) => {
 
-    const outcomeText = outcome === "draw" ? "It's a draw" : `The winner is ${outcome} team!`;
+    const outcomeText = outcome === "draw" ? "It's a draw" : `The winner is the ${outcome} team!`;
 
     if (!isOpen) {
         return null;
     }
     return (
-        <div style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'rgba(0, 0, 0, 0.7)'
-        }}>
-            <div style={{
-                backgroundColor: 'white',
-                padding: '20px',
-                borderRadius: '10px'
-            }}>
-                <h2>Game Over</h2>
-                <p>Is was a nice game</p>
-                <p>{outcomeText}</p>
+        <div
+            className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-70"
+        >
+            <div
+                className="bg-white rounded-lg shadow-2xl p-6 m-4 max-w-sm w-full text-center"
+            >
+                <p className="text-2xl font-bold text-red-600">Game Over</p>
 
-                <button onClick={onRestart}>Restart Game</button>
+                <p className="my-4 text-lg text-gray-700">
+                    It was a <span className="font-bold text-purple-600">legendary</span> battle!
+                </p>
+
+                <p className="my-4 text-md">
+                    {outcomeText}
+                </p>
+
+                <button
+                    className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full
+                    focus:outline-none focus:shadow-outline
+                    hover:shadow-lg hover:scale-105 transition duration-300 ease-in-out"
+                    onClick={onRestart}
+                >
+
+                    Restart Game
+                </button>
             </div>
         </div>
     );
--- src/components/greetingsModal.tsx
+++ src/components/greetingsModal.tsx
@@ -29,7 +29,7 @@ const GreetingsModal = (
 
                 <div className="flex justify-center">
                     <button
-                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full
+                        className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full
                         focus:outline-none focus:shadow-outline
                         hover:shadow-lg hover:scale-105 transition duration-300 ease-in-out
                         "
